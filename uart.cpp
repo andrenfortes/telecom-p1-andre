be214cf43a76090d5ca00adcf121735381d3f070
André Fortes <andrefortes2006@gmail.com>
2024-07-23 16:13:06 -0300

    atualizações uart.cpp e uart.hpp

--- uart.cpp
+++ uart.cpp
@@ -1,8 +1,69 @@
 #include "uart.hpp"
 
-void UART_RX::put_samples(const unsigned int *buffer, unsigned int n)
+void UART_RX::put_samples(const unsigned int* buffer, unsigned int n)
 {
-    // seu código aqui
+    for (unsigned int i = 0; i < n; i++) {
+        sample_buffer.push_back(buffer[i]);
+
+        switch (state) {
+        case WAIT_FOR_START:
+            if (sample_buffer.size() >= 30) {
+                if (check_start_bit()) {
+                    state = RECEIVE_BITS;
+                    bit_count = 0;
+                    sample_count = 0;
+                    received_byte = 0;
+                }
+                sample_buffer.pop_front();
+            }
+            break;
+
+        case RECEIVE_BITS:
+            if (sample_count == SAMPLES_PER_BIT / 2) {
+                if (bit_count < 8) {
+                    received_byte >>= 1;
+                    if (sample_buffer.front() == 1) {
+                        received_byte |= 0x80;
+                    }
+                    bit_count++;
+                }
+                else {
+                    state = CHECK_STOP;
+                }
+            }
+            sample_count++;
+            if (sample_count >= SAMPLES_PER_BIT) {
+                sample_count = 0;
+                sample_buffer.pop_front();
+            }
+            break;
+
+        case CHECK_STOP:
+            if (sample_count == SAMPLES_PER_BIT / 2) {
+                if (sample_buffer.front() == 1) {
+                    get_byte(received_byte);
+                }
+                state = WAIT_FOR_START;
+            }
+            sample_count++;
+            if (sample_count >= SAMPLES_PER_BIT) {
+                sample_count = 0;
+                sample_buffer.pop_front();
+            }
+            break;
+        }
+    }
+}
+
+bool UART_RX::check_start_bit()
+{
+    int low_samples = 0;
+    for (int i = 0; i < 30; i++) {
+        if (sample_buffer[i] == 0) {
+            low_samples++;
+        }
+    }
+    return low_samples >= START_BIT_THRESHOLD;
 }
 
 void UART_TX::put_byte(uint8_t byte)
@@ -17,7 +78,7 @@ void UART_TX::put_byte(uint8_t byte)
     samples_mutex.unlock();
 }
 
-void UART_TX::get_samples(unsigned int *buffer, unsigned int n)
+void UART_TX::get_samples(unsigned int* buffer, unsigned int n)
 {
     samples_mutex.lock();
     std::vector<unsigned int>::size_type i = 0;
